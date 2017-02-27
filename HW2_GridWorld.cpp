09becf7ccf9d50a0c6804445f09de3a80a0016ab
natbrauner <natan@nevada.unr.edu>
2017-02-26 21:13:00 -0800

    ver 2

--- HW2_GridWorld.cpp
+++ HW2_GridWorld.cpp
@@ -45,18 +45,19 @@ public:
 	//state, not nessasary but could be useful for project Beta
 };
 
-/*void bump(agent* pA,domain* pG) { //
+void bump(agent* pA,domain* pG) { //
 	//use if statement to deal with when the agent leaves the domain
 	int i;
 	while (pG->xmax<pA->Ax||pG->xmin>pA->Ax||pG->ymax<pA->Ay||pG->ymin>pA->Ay){
 			int Ax(i) = Ax(i - 1);
 			int Ay(i) = Ay(i - 1);
 	}
-}*/
+}
 
 void move(agent* pL, domain* pG) {
 	//write a program too move the agent within the domain
 	while (Ax != Gx && Ay != Gy) {
+		cout << "The coordinates of the agent (x,y)=(" << Ax << "," << Ay << ")" << endl;
 		int dir;
 		cout << "Press 1 (up), 2 (down), 3 (left), 4 (right) to choose a direction..." << endl;
 		cin >> dir;
@@ -72,36 +73,16 @@ void move(agent* pL, domain* pG) {
 				break;
 			default:
 				cout << "Sorry, that is not a direction." << endl;
-				cout << "The coordinates of the agent (x,y)=(" << Ax << "," << Ay << ")" << endl;
 			}
 			cout << "Congratulations, you have reached the goal!" << endl;
 	}
 	
 }
+
 int main()
 {
-	while (Ax != Gx && Ay != Gy) {
-		int dir;
-		cout << "Press 1 (up), 2 (down), 3 (left), 4 (right) to choose a direction..." << endl;
-		cin >> dir;
-		switch (dir) {
-		case 1:
-			Ay++;
-		case 2:
-			Ay--;
-		case 3:
-			Ax--;
-		case 4:
-			Ax++;
-			break;
-		default:
-			cout << "Sorry, that is not a direction." << endl;
-			cout << "The coordinates of the agent (x,y)=(" << Ax << "," << Ay << ")" << endl;
-		}
-		cout << "Congratulations, you have reached the goal!" << endl;
 
-
-		return 0;
-	}
+	//I am tring to run my move() and bump() functions in the main how do you do that?
+	return 0;
 }
 
